9ca7d9a5393d78fc96cf68c164033fc847e87ce0
Darrel Toledo <darreltoledo@icloud.com>
2020-03-31 21:52:38 +0100

    Local storage added

--- singleproductdisplay.js
+++ singleproductdisplay.js
@@ -1,8 +1,9 @@
 // Accessing DOM
 const camera = document.querySelector('#camera');
 const params = new URLSearchParams(window.location.search);
-const id = params.get("camera");
-console.log(camera, params, id, window.location.search)
+const id = params.get('camera');
+console.log(camera, params, id, window.location.search);
+const cartTotal = document.querySelector(".cartTotal");
 
 //Api request & Dynamic function
 
@@ -12,7 +13,7 @@ function getCameras() {
       return res.json()
     })
     .then(function (camera) {
-      console.log(camera)
+      console.log(camera);
       let lenses = "";
       camera.lenses.forEach((lens) => {
         lenses += `<option>${lens}</option>`
@@ -28,59 +29,35 @@ function getCameras() {
       <p class="card-text">${camera.description}</p>
       <p class="card-text">Price: $ ${camera.price}</p>
       <p class="card-text">Lense selection</p>
-      <select class="form-control" id="lenses-option"><option>${lenses}</option></select>
-      <a class="btn btn-primary my-2 add-cart">Add to the cart</a>
+      <select class="form-control" id="lenses-option">
+      <option value="choose">Please choose a lense${lenses}</option></select>
+      <a class="btn btn-primary my-2" id="addItemToCart">Add to the cart</a>
       </div>
       </div>
       </article>
   `;
-      let carts = document.querySelectorAll('.add-cart');
-
-      console.log(carts);
-      for (let i = 0; i < carts.length; i++) {
-        console.log(carts[i])
-        carts[i].addEventListener('click', (evt) => {
-          evt.preventDefault();
-          console.log('click');
-        })
-      }
-      // sample exercise
-      const test = document.getElementById('test');
-      console.log(test);
-      test.addEventListener('click', function () {
-        console.log('test click');
-      })
-
-      window.localStorage.setItem('test', 'store');
-
-      const check = window.localStorage.getItem('test');
-      console.log(check);
-      // End of exercises
-
-
-      // Storing Data in Browsers
-
-      function localStorage(product, link) {
-        let productData = {
-          name: product.name,
-          price: product.price,
-          id: product._id,
-          qty: 1
-        };
-
-        const itemsArray = window.localStorage.getItem('#cart') ?
-          JSON.parse(window.localStorage.getItem('#cart')) : [];
-        itemsArray.push(productData);
-
-        window.localStorage.setItem('cart', JSON.stringify(itemsArray));
-        console.log('localStorage');
-      }
-      localStorage(camera)
-
       console.log(output)
       document.getElementById('camera').innerHTML = output;
+
+      // Local Storage in Browsers
+      const carts = document.getElementById('addItemToCart');
+      console.log(carts);
+
+      carts.addEventListener('click', function () {
+        console.log('clicked cart');
+
+        const lense = document.getElementById('lenses-option').value;
+        console.log(lense);
+        let addItemToCart = {
+          name: camera.name,
+          price: camera.price,
+          id: camera._id,
+          lense: lense,
+        };
+
+        window.localStorage.setItem('carts', JSON.stringify(addItemToCart));
+        console.log(addItemToCart);
+      })
     })
 }
-
 getCameras()
-
