ec572b534f9efe47b2066656befe20e7833b0e15
Darrel Toledo <darreltoledo@icloud.com>
2020-04-20 14:04:18 +0100

    Order ID and Form are now reponsive

--- contact-form.js
+++ contact-form.js
@@ -1,24 +1,40 @@
-// Accessing DOM from form
-const myForm = document.getElementById('myForm');
-const submitBtn = document.getElementById('submit-button');
+// Get form elements
+let myForm = document.getElementById('myForm');
+let firstNameInput = document.getElementById('first-name').value;
+let lastNameInput = document.getElementById('last-name').value;
+let addressInput = document.getElementById('address').value;
+let cityInput = document.getElementById('city').value;
+let emailInput = document.getElementById('email').value;
+let submitButton = document.getElementById('submit-button');
 
-console.log(submitBtn, myForm, formData);
+console.log(firstNameInput);
 
 function inputValidation() {
-  fetch('http://localhost:3000/api/cameras/order', { method: 'post', body: {} })
+  fetch('http://localhost:3000/api/cameras/order', {
+    method: 'POST',
+    body: JSON.stringify({ firstNameInput: firstNameInput, lastNameInput: lastNameInput, address: address, city: city, email: email })
+  })
     .then((res) => res.json())
-    .then((data) => {
-      let output = ""
-      console.log(data);
-      document.getElementById('output').innerHTML = output;
-    })
-};
+    .then((data) =>
+      console.log(data))
+}
 
-submitBtn.addEventListener('click',
+submitButton.addEventListener('click',
   function (e) {
-    e.preventDefault()
-    console.log(submitBtn);
+    e.preventDefault();
+    console.log(submitButton);
   }
 )
+
+// User input validation. The RegExp constructor creates a regular expression object for matching text with a pattern format.
+emailInput.addEventListener('input', (e) => {
+  let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (emailInput.value.match(mailformat)) {
+    submitButton.removeAttribute('disabled');
+  }
+  else {
+    submitButton.setAttribute('disabled');
+  }
+});
 
 inputValidation()
