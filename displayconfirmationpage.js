7f7e4a2eb032a11b3d8974db74f7de02689438d8
Darrel Toledo <darreltoledo@icloud.com>
2020-04-27 11:46:41 +0100

    Submit data form page to confirmation page

--- displayconfirmationpage.js
+++ displayconfirmationpage.js
@@ -1,3 +1,17 @@
-function getOrders() {
-  console.log(getOrders);
+
+// Retrieve Total price and order details by Url query
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+const price = params.get("price");
+const totalPrice = document.getElementById('total-price');
+const responseOrderId = getElementById('response-id');
+console.log(params, id, price);
+
+
+function confirmedOrders() {
+  console.log(confirmedOrders);
+  output += `
+  `;
 }
+
+confirmedOrders()
